832b2024a7dbefba4ea35e85976d295e59a447c6
Alonso Garro Ruiz <gustavogr0217@gmail.com>
2023-11-18 00:50:46 -0600

    Metodo CargarImagenes() creado en ManteAlojamiento

--- Proyecto_DreamPlace/Paginas/ManteAlojamiento.aspx.cs
+++ Proyecto_DreamPlace/Paginas/ManteAlojamiento.aspx.cs
@@ -11,27 +11,32 @@ namespace Proyecto_DreamPlace.Paginas
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            int ID = 39;
+            
             if (!IsPostBack)
             {
                 if (Session["Correo"] != null)
                 {
                     string correo = Session["Correo"].ToString();
+                    CargarImagenes();
                 }
-                List<byte[]> listaimagenes = ConexionBD.ObtenerImagenesPorIdInmueble(ID);
-                int limite = Math.Min(listaimagenes.Count, 5);
-                for(int i = 0; i < limite; i++)
+                
+            }
+        }
+        public void CargarImagenes()
+        {
+            int ID = 39;
+            List<byte[]> listaimagenes = ConexionBD.ObtenerImagenesPorIdInmueble(ID);
+            int limite = Math.Min(listaimagenes.Count, 5);
+            for (int i = 0; i < limite; i++)
+            {
+                string imagenid = "Image" + (i + 1);
+                System.Web.UI.WebControls.Image ImageC = FindControl(imagenid) as System.Web.UI.WebControls.Image;
+                if (ImageC != null)
                 {
-                    string imagenid = "Image" + (i + 1);
-                    System.Web.UI.WebControls.Image ImageC = FindControl(imagenid) as System.Web.UI.WebControls.Image;
-                    if (ImageC!=null)
-                    {
-                        ImageC.ImageUrl = "data:image/png;base64," + Convert.ToBase64String(listaimagenes[i]);
-                        ImageC.CssClass = "imagen-estilo";
-                    }
+                    ImageC.ImageUrl = "data:image/png;base64," + Convert.ToBase64String(listaimagenes[i]);
+                    ImageC.CssClass = "imagen-estilo";
                 }
             }
         }
-
     }
 }

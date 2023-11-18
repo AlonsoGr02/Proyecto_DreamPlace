02203ec8591e42ef8e4380fe6bef275be806f0ac
Alonso Garro Ruiz <gustavogr0217@gmail.com>
2023-11-17 21:02:55 -0600

    Estilos para la pantalla de mantenimiento

--- Proyecto_DreamPlace/Paginas/ManteAlojamiento.aspx
+++ Proyecto_DreamPlace/Paginas/ManteAlojamiento.aspx
@@ -6,9 +6,10 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Favoritos</title>
+    <title>Mantenimiento</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-..." />
-    <link rel="StyleSheet" href="../Estilos/Css_ManteAlojamiento.css" type="text/css"/>
+    <link href="../Estilos/Css_ManteA.css" rel="stylesheet" />
+
 </head>
 <body>
     <form id="form1" runat="server">
@@ -25,11 +26,50 @@
                 <div class="flex-container">
                     <a href="CuentaAnfitrion.aspx?Correo=<%= Session["Correo"] %>">Cuenta</a>
                     <asp:Label ID="Label1" runat="server">  >  </asp:Label>
-                    <asp:Label ID="Labelseparador" runat="server" Text="Mantenimiento de tu Alojamiento "></asp:Label>
+                    <asp:Label ID="Labelseparador" runat="server" Text="Mantenimiento de tu Inmueble "></asp:Label>
+                    <hr />
                 </div>
-                <h1>Mantenimiento de tu Alojamiento</h1>
+                <h1>Mantenimiento de tu Inmueble</h1>
+            </div>
+            
+            <br />
+            <asp:DropDownList ID="ddlAlojamientos" runat="server"></asp:DropDownList><br />
+            <hr />
+            <div class="container-info">
+                <h2>Caracteristicas de tu Inmueble</h2>
+                <asp:Label ID="Label2" runat="server" Text="Nombre:"></asp:Label><br />
+                <asp:TextBox ID="txtNombre" runat="server" AutoCompleteType="Disabled"></asp:TextBox><br />
+
+                <asp:Label ID="Label3" runat="server" Text="Descripción:"></asp:Label><br />
+                <asp:TextBox ID="txtDescripcion" runat="server" AutoCompleteType="Disabled"></asp:TextBox><br />
+
+                <asp:Label ID="Label4" runat="server" Text="Cantidad de Personas:"></asp:Label><br />
+                <asp:TextBox ID="txtCantidadP" runat="server" AutoCompleteType="Disabled"></asp:TextBox><br />
+
+                <asp:Label ID="Label5" runat="server" Text="Cantidad de Dormitorios:"></asp:Label><br />
+                <asp:TextBox ID="txtCantidadD" runat="server" AutoCompleteType="Disabled"></asp:TextBox><br />
+
+                <asp:Label ID="Label6" runat="server" Text="Cantidad de Baños:"></asp:Label><br />
+                <asp:TextBox ID="txtCantidadB" runat="server" AutoCompleteType="Disabled"></asp:TextBox><br />
+
+                <asp:Label ID="Label7" runat="server" Text="Cantidad de Camas:"></asp:Label><br />
+                <asp:TextBox ID="txtCantiCamas" runat="server" AutoCompleteType="Disabled"></asp:TextBox><br />
+
+                <asp:Label ID="Label8" runat="server" Text="Categoria:"></asp:Label><br />
+                <asp:DropDownList ID="ddlCategoria" runat="server" ></asp:DropDownList><br />
+
+                <asp:Label ID="Label9" runat="server" Text="Estado:"></asp:Label><br />
+                <asp:DropDownList ID="ddlEstado" runat="server"></asp:DropDownList><br />
+
+                <asp:Label ID="Label10" runat="server" Text="Descripción del Estado :"></asp:Label><br />
+                <asp:TextBox ID="txtDescripcionEstado" runat="server" AutoCompleteType="Disabled"></asp:TextBox><br />
             </div>
             <br />
+            <br />
+            <hr />
+            <br />
+            <asp:Button ID="btnGuardar"  class="fa-solid" runat="server" Text="Guardar" />
+
         </div>
     </form>
 </body>
--- Proyecto_DreamPlace/Paginas/ManteAlojamiento.aspx.designer.cs
+++ Proyecto_DreamPlace/Paginas/ManteAlojamiento.aspx.designer.cs
@@ -40,5 +40,185 @@ namespace Proyecto_DreamPlace.Paginas
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Labelseparador;
+
+        /// <summary>
+        /// Control ddlAlojamientos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlAlojamientos;
+
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+
+        /// <summary>
+        /// Control Label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+
+        /// <summary>
+        /// Control txtDescripcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDescripcion;
+
+        /// <summary>
+        /// Control Label4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+
+        /// <summary>
+        /// Control txtCantidadP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCantidadP;
+
+        /// <summary>
+        /// Control Label5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+
+        /// <summary>
+        /// Control txtCantidadD.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCantidadD;
+
+        /// <summary>
+        /// Control Label6.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+
+        /// <summary>
+        /// Control txtCantidadB.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCantidadB;
+
+        /// <summary>
+        /// Control Label7.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label7;
+
+        /// <summary>
+        /// Control txtCantiCamas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCantiCamas;
+
+        /// <summary>
+        /// Control Label8.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label8;
+
+        /// <summary>
+        /// Control ddlCategoria.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlCategoria;
+
+        /// <summary>
+        /// Control Label9.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label9;
+
+        /// <summary>
+        /// Control ddlEstado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlEstado;
+
+        /// <summary>
+        /// Control Label10.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label10;
+
+        /// <summary>
+        /// Control txtDescripcionEstado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDescripcionEstado;
+
+        /// <summary>
+        /// Control btnGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardar;
     }
 }

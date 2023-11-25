355492bfce113b5e7a1238779f2a8045f4dcf3ec
Esteban AS <arayaesteban25@gmail.com>
2023-11-25 15:58:40 -0600

    Agregue Pantalla y sus estilos para Verificacion de Identidad

---
+++ Proyecto_DreamPlace/Paginas/VerficarIdentidad.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Proyecto_DreamPlace.Paginas
+{
+    public partial class VerficarIdentidad : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnValidar_Click(object sender, EventArgs e)
+        {
+            byte[] imagenFrontal = null;
+            byte[] imagenTrasera = null;
+
+            // Validar que los archivos estén adjuntos siempre
+            if (!FileUploadFrontal.HasFile || !FileUploadTrasera.HasFile)
+            {
+                lblRespu.Text = "Debe adjuntar ambos archivos";
+                return;
+            }
+
+            if (FileUploadFrontal.HasFile)
+            {
+                imagenFrontal = ConvertirImagenABytes(FileUploadFrontal.PostedFile);
+                lblInfo.Text = "Adjunta la foto trasera de la cédula";
+                lblInfoTrasera.Text = "";
+            }
+            if (FileUploadTrasera.HasFile)
+            {
+                imagenTrasera = ConvertirImagenABytes(FileUploadTrasera.PostedFile);
+            }
+
+            lblRespu.Text = "mostar respu aqui";
+        }
+
+        private byte[] ConvertirImagenABytes(HttpPostedFile file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                byte[] imagenEnBytes = new byte[file.ContentLength];
+                file.InputStream.Read(imagenEnBytes, 0, file.ContentLength);
+                return imagenEnBytes;
+            }
+            return null;
+        }
+    }
+}
---
+++ Proyecto_DreamPlace/Paginas/VerficarIdentidad.aspx.designer.cs
@@ -0,0 +1,98 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Proyecto_DreamPlace.Paginas
+{
+
+
+    public partial class VerficarIdentidad
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control Labelseparador.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Labelseparador;
+
+        /// <summary>
+        /// Control lblInfo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblInfo;
+
+        /// <summary>
+        /// Control FileUploadFrontal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload FileUploadFrontal;
+
+        /// <summary>
+        /// Control lblInfoTrasera.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblInfoTrasera;
+
+        /// <summary>
+        /// Control FileUploadTrasera.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload FileUploadTrasera;
+
+        /// <summary>
+        /// Control btnValidar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnValidar;
+
+        /// <summary>
+        /// Control lblRespu.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblRespu;
+    }
+}

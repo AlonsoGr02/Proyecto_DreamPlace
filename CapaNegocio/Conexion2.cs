220d6221184a1e8210c1bfaf8b8252e66492f22e
Esteban AS <arayaesteban25@gmail.com>
2023-12-04 17:35:34 -0600

    Recepcion de Denuncias Funcionando

--- CapaNegocio/Conexion2.cs
+++ CapaNegocio/Conexion2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaNegocio.Models;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -32,5 +33,77 @@ namespace CapaNegocio
 
             return dt;
         }
+
+        public DataTable ObtenerDenunciasPorCedula2(string cedula)
+        {
+            DataTable dtDenuncias = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(cadenaConexion))
+            {
+                using (SqlCommand cmd = new SqlCommand("ObtenerDenunciasPorCedula", connection))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    // Añadir el parámetro de cédula al comando
+                    cmd.Parameters.AddWithValue("@Cedula", cedula);
+
+                    try
+                    {
+                        connection.Open();
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            dtDenuncias.Load(reader);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        // Manejar excepciones según tus necesidades
+                        Console.WriteLine("Error al obtener las denuncias: " + ex.Message);
+                    }
+                }
+            }
+
+            return dtDenuncias;
+        }
+
+        public List<Denuncia> ObtenerDenunciasPorCedula(string cedula)
+        {
+            List<Denuncia> denuncias = new List<Denuncia>();
+
+            using (SqlConnection connection = new SqlConnection(cadenaConexion))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT D.Denuncia, I.Nombre FROM Denuncias D INNER JOIN Inmuebles I ON D.IdInmueble = I.IdInmueble WHERE D.IdCedula = @Cedula;", connection))
+                {
+                    cmd.Parameters.AddWithValue("@Cedula", cedula);
+
+                    try
+                    {
+                        connection.Open();
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Denuncia denuncia = new Denuncia
+                                {
+                                    ContenidoDenuncia = reader["Denuncia"].ToString(),
+                                    NombreInmueble = reader["Nombre"].ToString()
+                                };
+
+                                denuncias.Add(denuncia);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error al obtener las denuncias: " + ex.Message);
+                    }
+                }
+            }
+
+            return denuncias;
+        }
+
     }
 }
---
+++ CapaNegocio/Models/Denuncia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio.Models
+{
+    public class Denuncia
+    {
+        public string NombreInmueble { get; set; }
+        public string ContenidoDenuncia { get; set; }
+    }
+}
